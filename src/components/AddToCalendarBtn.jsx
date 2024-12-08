94f7de845b813450395d2423b856fc97edb43cc7
roodbaraky <kooroshr@hotmail.com>
2024-12-08 16:49:12 +0000

    Removed Google login from add-to-calendar due to Google Js origin policy

--- src/components/AddToCalendarBtn.jsx
+++ src/components/AddToCalendarBtn.jsx
@@ -8,11 +8,14 @@ function AddToCalendar({ event }) {
   const getCalendarUrl = () => {
     const formattedStart = start_datetime.replace(/-|:|\.\d\d\d/g, "");
     const formattedEnd = end_datetime.replace(/-|:|\.\d\d\d/g, "");
-    return `https://calendar.google.com/calendar/r/eventedit?text=${encodeURIComponent(
+    const showingURL = window.location.href
+    const details = description + "%0D%0D" + `Showing link: ${showingURL}`
+
+    return `https://calendar.google.com/calendar/event?action=TEMPLATE&text=${
       title
-    )}&dates=${formattedStart}/${formattedEnd}&details=${encodeURIComponent(
-      description
-    )}&location=${encodeURIComponent(location)}`;
+    }&dates=${formattedStart}/${formattedEnd}&details=${
+      details
+    }&location=${location}`;
   };
 
   const handleAddToCalendar = () => {
@@ -25,11 +28,12 @@ function AddToCalendar({ event }) {
       id="AddToCalendar"
       className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700 transition-colors duration-300"
       onClick={() => {
-        if (googleAccessToken) {
-          handleAddToCalendar();
-        } else {
-          loginWithGoogle();
-        }
+        // if (googleAccessToken) {
+        //   handleAddToCalendar();
+        // } else {
+        //   loginWithGoogle();
+        // }
+        handleAddToCalendar()
       }}
     >
       Add to Calendar

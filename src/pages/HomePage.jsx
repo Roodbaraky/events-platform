e0f9858b0162ad45c6ab92c8f1e32c5dd83abd08
roodbaraky <kooroshr@hotmail.com>
2024-12-18 13:58:51 +0000

    Added login modal validation to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,29 @@
 import { useNavigate } from "react-router-dom";
+import { useSession } from "../contexts/UserContext";
 
 function HomePage() {
-    const navigate = useNavigate()
-    return (
-      <section className="flex flex-col items-center justify-center flex-grow gap-4">
-        <h1 className="text-7xl">Welcome.</h1>
-        <div className="flex gap-2">
-          <a className="btn btn-primary" onClick={()=>navigate("/events")}>Browse Events</a>
-          <a className="btn btn-outline" onClick={()=>navigate("/mypage")}>My page</a>
-        </div>
-      </section>
-    );
-  }
-  
-  export default HomePage;
-  
+  const navigate = useNavigate();
+  const { session } = useSession();
+  return (
+    <section className="flex flex-col items-center justify-center flex-grow gap-4">
+      <h1 className="text-7xl">Welcome.</h1>
+      <div className="flex gap-2">
+        <a className="btn btn-primary" onClick={() => navigate("/events")}>
+          Browse Events
+        </a>
+        <a
+          className="btn btn-outline"
+          onClick={() => {
+            session
+              ? navigate("/mypage")
+              : document.getElementById("login")?.showModal();
+          }}
+        >
+          My page
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default HomePage;

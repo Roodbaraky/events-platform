73e1bbda84ce6ac4df886ff57f5b25b2adece893
roodbaraky <kooroshr@hotmail.com>
2024-12-23 13:42:40 +0000

    Removed legacy google auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
@@ -6,10 +5,8 @@ import "./index.css";
 import { UserProvider } from "./contexts/UserContext.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ErrorProvider } from "./contexts/ErrorContext.jsx";
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId={clientId}>
     <QueryClientProvider client={queryClient}>
       <ErrorProvider>
         <UserProvider>
@@ -19,5 +16,4 @@ createRoot(document.getElementById("root")).render(
         </UserProvider>
       </ErrorProvider>
     </QueryClientProvider>
-  </GoogleOAuthProvider>,
 );
